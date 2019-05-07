c233f5f9aa9bbbbde55b80d6bdb6e7667d4cf9c5
Diego Lopez Sarmiento <diego.lopezsarmiento@gmail.com>
2019-05-07 13:31:27 -0300

    initial set up for angularjs

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@
 
     angular.module('myApp', [])
         .controller('NewTabController', function ($scope, $timeout) {
-            $scope.clock = '12:00';
+            $scope.clock;
 
-            function udpateClock() {
+            var udpateClock = function() {
                 $scope.clock = new Date();
+                $timeout(function(){
+                    udpateClock();
+                }, 1000);
             }
-
             udpateClock();
-
-
         });
 })();
